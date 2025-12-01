9db81f4640840a679e31192080df695b3c957339
Marques <gaston.marques@gmail.com>
2025-12-01 11:17:18 -0500

    init

---
+++ src/test/java/com/github/gastonm1/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gastonm1.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
