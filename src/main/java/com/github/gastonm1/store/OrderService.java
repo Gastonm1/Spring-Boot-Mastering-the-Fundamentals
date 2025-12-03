7750d490c741d3aaf1da7281098e5c2882cf378a
Marques <gaston.marques@gmail.com>
2025-12-03 16:38:58 -0500

    Spring Boot: Mastering The Fundamentals - The Spring IoC Container

--- src/main/java/com/github/gastonm1/store/OrderService.java
+++ src/main/java/com/github/gastonm1/store/OrderService.java
@@ -3,17 +3,11 @@ package com.github.gastonm1.store;
 public class OrderService {
     private PaymentService paymentService;
 
-//    public OrderService(PaymentService paymentService) {
-//        this.paymentService = paymentService;
-//    }
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
 
     public void placeOrder() {
        paymentService.processPayment(10);
-    }
-
-    // You can inject a dependency via setters but the more common way is using the constructor
-    // Here is an example of using the setter
-    public void setPaymentService(PaymentService paymentService) {
-        this.paymentService = paymentService;
     }
 }
