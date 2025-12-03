417bf39590da9ca4a2c5d7d6f09dab8e405b9d15
Marques <gaston.marques@gmail.com>
2025-12-03 17:01:02 -0500

    Spring Boot: Mastering The Fundamentals - Configuring Beans using Annotations

--- src/main/java/com/github/gastonm1/store/StoreApplication.java
+++ src/main/java/com/github/gastonm1/store/StoreApplication.java
@@ -5,14 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.annotation.Order;
 
-// Spring IoC Container
-// One of the core features of the Spring framework is the ability to create objects and inject them into our classes
-// IOC Container: This container is responsible for managing the objects in our application
-// In Spring Terminology: These managed objects are known as "Beans"
-// So a "Bean" is just a regular Java object that is managed by Spring
-// Spring takes care of creating it, injecting its dependencies, and managing its life cycle.
-// IOC: Inversional of Control
-// Inverts the control of creating objects and managing dependencies.
 
 @SpringBootApplication
 public class StoreApplication {
