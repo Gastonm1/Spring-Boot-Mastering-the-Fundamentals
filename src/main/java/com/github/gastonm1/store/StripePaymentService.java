7374a7850535f6625fe188ce28f3fc24e68f1220
Marques <gaston.marques@gmail.com>
2025-12-03 16:17:48 -0500

    Spring Boot: Mastering The Fundamentals - What is a Dependency Injection

---
+++ src/main/java/com/github/gastonm1/store/StripePaymentService.java
@@ -0,0 +1,9 @@
+package com.github.gastonm1.store;
+
+public class StripePaymentService implements PaymentService {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("STRIPE");
+        System.out.println("Amount: " + amount);
+    }
+}
