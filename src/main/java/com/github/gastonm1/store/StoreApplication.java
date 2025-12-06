c16623335ef9acdac24dbaac92d3dfcff54147cc
Marques <gaston.marques@gmail.com>
2025-12-06 08:41:28 -0500

    Spring Boot: Mastering The Fundamentals - Database Integration with Spring Data

--- src/main/java/com/github/gastonm1/store/StoreApplication.java
+++ src/main/java/com/github/gastonm1/store/StoreApplication.java
@@ -3,16 +3,11 @@ package com.github.gastonm1.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.core.annotation.Order;
-
 
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-        var resource = context.getBean(HeavyResource.class);
-//        var orderService = context.getBean(OrderService.class);
-//        orderService.placeOrder();
 	}
 }
