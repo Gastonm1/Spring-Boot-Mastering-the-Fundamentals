33b4ce6b1c8715cd059e919377090da969193829
Marques <gaston.marques@gmail.com>
2025-12-09 15:22:26 -0500

    Spring Boot: Mastering The Fundamentals - Derived Query Methods, Writing Custom Queries

--- src/main/java/com/github/gastonm1/store/repositories/ProductRepository.java
+++ src/main/java/com/github/gastonm1/store/repositories/ProductRepository.java
@@ -3,5 +3,37 @@ package com.github.gastonm1.store.repositories;
 import com.github.gastonm1.store.entities.Product;
 import org.springframework.data.repository.CrudRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface ProductRepository extends CrudRepository<Product, Long> {
+
+    // String
+    List<Product> findByName(String name);
+    List<Product> findByNameLike(String name);
+    List<Product> findByNameNotLike(String name);
+    List<Product> findByNameContaining(String name);
+    List<Product> findByNameEndingWith(String name);
+    List<Product> findByNameEndingWithIgnoreCase(String name);
+
+    // Numbers
+    List<Product> findByPrice(BigDecimal price);
+    List<Product> findByPriceGreaterThan(BigDecimal price);
+    List<Product> findByPriceLessThan(BigDecimal price);
+    List<Product> findByPriceBetween(BigDecimal price1, BigDecimal price2);
+    List<Product> findByPriceGreaterThanEqual(BigDecimal price);
+
+    // Null
+    List<Product> findByDescriptionNull();
+    List<Product> findByDescriptionNotNull();
+
+    // Multiple Conditions
+    List<Product> findByDescriptionNullAndNameNull();
+
+    //Sort (Order By)
+    List<Product> findByNameOrderByPrice(String name);
+
+    //Limit (Top/First)
+    List<Product> findTop5ByNameOrderByPrice(String name);
+    List<Product> findFirst5ByNameLikeOrderByPrice(String name);
 }
